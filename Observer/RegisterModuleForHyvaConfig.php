5b58144e92e1b93ea385e2a7bc64e2242e3a2881
WorldlineMagentoTeam <plugin.worldline@amasty.com>
2023-03-02 17:42:00 +0100

    Release 1.0.0

---
+++ Observer/RegisterModuleForHyvaConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Worldline\ThemeHyva\Observer;
+
+use Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Event\Observer as Event;
+use Magento\Framework\Event\ObserverInterface;
+
+class RegisterModuleForHyvaConfig implements ObserverInterface
+{
+    /**
+     * @var ComponentRegistrar
+     */
+    private $componentRegistrar;
+
+    public function __construct(ComponentRegistrar $componentRegistrar)
+    {
+        $this->componentRegistrar = $componentRegistrar;
+    }
+
+    public function execute(Event $event): void
+    {
+        $config = $event->getData('config');
+        $extensions = $config->hasData('extensions') ? $config->getData('extensions') : [];
+
+        $path = $this->componentRegistrar->getPath(ComponentRegistrar::MODULE, 'Worldline_ThemeHyva');
+
+        // Only use the path relative to the Magento base dir
+        $extensions[] = ['src' => substr($path, strlen(BP) + 1)];
+
+        $config->setData('extensions', $extensions);
+    }
+}
